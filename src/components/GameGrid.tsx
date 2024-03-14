11480200ea964ec2374f27a7406ca679d27e2a35
elham <elham_gholami@comp.iuct.ac.ir>
2024-03-14 16:41:46 +0330

    making a costum hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,26 +1,16 @@
-import { useEffect, useState } from "react";
-import apiClients from "../services/api-clients";
-interface Game {
-  name: string;
-  id: number;
-}
-
-interface responseType {
-  results: Game[];
-}
+import { Text } from "@chakra-ui/react";
+import { useGames } from "../hooks/useGames";
 
 export function GameGrid() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClients
-      .get<responseType>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch(err => setError('Some error occured'+err.message));
-  });
-
-  return(
-    <ul>{games.map(game => <li key={game.id}>{game.name}</li>)} </ul>
-  )
+  const { games, error } = useGames();
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}{" "}
+      </ul>
+    </>
+  );
 }
