dd7b243fc4cb9d36263b82630c204dd43c1e1adf
elham <elham_gholami@comp.iuct.ac.ir>
2024-03-17 23:07:23 +0330

    refactoring genres

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       </GridItem>
       <Show above='lg'>
 
-      <GridItem area="aside">
+      <GridItem area="aside" paddingX={5}>
         <GenreList/>
       </GridItem>
       </Show>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,29 @@
+import { HStack, Img, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres"
 
 export function GenreList()
 {
     const {data} = useGenres();
     return(
-        <>
-        {
-            data.map(genre => <li>{genre.name}</li>)
-        }
-        </>
+        <List>
+            {
+                data.map (
+                    genre=> 
+                    (
+                        <ListItem key={genre.id} paddingY='10px'>
+
+                            <HStack>
+                                <Img boxSize='32px' borderRadius={8} src={genre.image_background}/>
+                                <Text fontSize='20px'> {genre.name} </Text>
+                            </HStack>
+                        </ListItem>
+
+                    )
+                )
+            }
+           
+
+        </List>
+      
     )
 }
