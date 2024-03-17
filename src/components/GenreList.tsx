97a32c93e523a93ddbf2f13eddfa2aed87aa225b
elham <elham_gholami@comp.iuct.ac.ir>
2024-03-17 23:12:30 +0330

    adding a loading spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,29 +1,20 @@
-import { HStack, Img, List, ListItem, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres"
+import { HStack, Img, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
 
-export function GenreList()
-{
-    const {data} = useGenres();
-    return(
-        <List>
-            {
-                data.map (
-                    genre=> 
-                    (
-                        <ListItem key={genre.id} paddingY='10px'>
-
-                            <HStack>
-                                <Img boxSize='32px' borderRadius={8} src={genre.image_background}/>
-                                <Text fontSize='20px'> {genre.name} </Text>
-                            </HStack>
-                        </ListItem>
-
-                    )
-                )
-            }
-           
-
-        </List>
-      
-    )
+export function GenreList() {
+  const { data, isLoading, error } = useGenres();
+  if (isLoading) return <Spinner></Spinner>;
+  if (error) return null;
+  return (
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="10px">
+          <HStack>
+            <Img boxSize="32px" borderRadius={8} src={genre.image_background} />
+            <Text fontSize="20px"> {genre.name} </Text>
+          </HStack>
+        </ListItem>
+      ))}
+    </List>
+  );
 }
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -2,31 +2,31 @@ import { useEffect, useState } from "react";
 import apiClients from "../services/api-clients";
 import { CanceledError } from "axios";
 
-
- 
-interface FetchResponse<T>
-{
-    results: T[];
+interface FetchResponse<T> {
+  results: T[];
 }
 
-export function useData <T>
-(endpoint: string) 
-{
+export function useData<T>(endpoint: string) {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [data, setData] = useState<T[]>([]);
-    const [error, setError] = useState("");
+  useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
+    apiClients
+      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .then((res) => {
+        setData(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError("Some error occured -details: " + err.message);
+        setIsLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
 
-    useEffect(() => {
-        const controller = new AbortController();
-        apiClients
-        .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
-        .then((res) => setData(res.data.results))
-        .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError("Some error occured -details: " + err.message);
-        });
-        return () => controller.abort();
-    }, []);
-
-  return { data, error };
+  return { data, error, isLoading };
 }
